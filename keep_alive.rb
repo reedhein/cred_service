a7200af25516735a59a4b900422dfca0bb40c9a9
voodoologic <headley.douglas@gmail.com>
2016-06-21 09:36:30 -0700

    update

--- keep_alive.rb
+++ keep_alive.rb
@@ -1,9 +1,11 @@
+require_relative '../global_utilities/global_utilities'
 class KeepAlive
   def initialize
     @sf_client  = Utils::SalesForce::Client.instance
     @box_client = Utils::Box::Client.instance
 
     loop do 
+      puts "calling services"
       @sf_client.query("SELECT id FROM Opportunity LIMIT 1")
       @box_client.root_folder_items
       sleep 3599
